9ef9b2116684d5eaf68fbfd4da40a9e66eb7f504
mountmestallion <160457151+mountmestallion@users.noreply.github.com>
2025-06-19 03:25:54 +0200

    Create Bot.js

---
+++ Bot/Bot.js
@@ -0,0 +1,4 @@
+Forms/registration _link.md. #Registreringslenke 
+
+Frivillig gutt:https://docs.google.com/forms/d/e/1FAIpQLSfbfgVM1giiBbRmGICYxF4CK2hb2T5fyDuD7fO2IxZdR1-d1w/viewform?usp=sharing&ouid=100253633813672091945
+Hestejente:  https://docs.google.com/forms/d/e/1FAIpQLSeZu443H6_1DgzWbV_P5YS2AMvZA-Sm-lOMYJVWH276s6TJ0Q/viewform?usp=dialog
